c4879ff29e7dee0745c795050346348c292f5290
patricktien <patrick1996112@gmail.com>
2022-08-29 14:57:22 -0700

    Install passort, google-oauth package

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config(); // for .env file
+require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var workoutsRouter = require('./routes/workouts');
+// var notesRouter = require('./routes/notes');
+// var performersRouter = require('./routes/performers');
 
 var app = express();
 
@@ -20,7 +24,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/workouts', workoutsRouter);
+// app.use('/', notesRouter);
+// app.use('/', performersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
